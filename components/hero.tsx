0e6f959e4ab76b97657142cf25c8e1b3283e79b4
PromiseUdo <info.promiseudo@gmail.com>
2025-08-12 08:21:01 +0100

    update

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,14 +8,14 @@ import Link from "next/link";
 
 const slides = [
   {
-    image: "/slide1.png",
+    image: "/slide1.webp",
     headline: "Built to excel and shine",
     subheading: "Euston University",
     primaryButton: "Apply",
     secondaryButton: "Learn more",
   },
   {
-    image: "/slide2.jpg",
+    image: "/slide2.webp",
     headline: "Designed for the 21st Century Learner",
     subheading:
       "Euston University offers a bold departure from the traditional university model.",
@@ -23,7 +23,7 @@ const slides = [
     secondaryButton: "Learn more",
   },
   {
-    image: "/slide3.png",
+    image: "/slide3.webp",
     headline: "World-Class Faculty and Learning Infrastructure",
     subheading:
       "Euston University prides itself on attracting seasoned academics & industry professionals.",
