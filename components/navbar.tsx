14bc21bc32cdd28b4887020fe0584a2f49ce39da
PromiseUdo <info.promiseudo@gmail.com>
2025-08-14 11:46:05 +0100

    update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -763,12 +763,18 @@ interface MenuSection {
 }
 
 const coursesCategory: CourseCategory[] = [
-  { title: "Facultiy of Allied Health Sciences", href: "/" },
-  { title: "Faculty of Natural and Applied Sciences", href: "/" },
-  { title: "Faculty of Computing", href: "/" },
+  {
+    title: "Facultiy of Allied Health Sciences",
+    href: "/faculties/faculty-of-allied-health-sciences",
+  },
+  {
+    title: "Faculty of Natural and Applied Sciences",
+    href: "/faculties/faculty-of-natural-and-applied-sciences",
+  },
+  { title: "Faculty of Computing", href: "/faculties/faculty-of-computing" },
   {
     title: "Faculty of Arts, Management and Social Sciences",
-    href: "/",
+    href: "/faculties/faculty-of-arts-management-and-social-sciences",
   },
 ];
 
